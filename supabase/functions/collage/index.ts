77886700ddd7236f449a9f2b5daacdf9d290a388
Bidyut Mahanta <bidyutmahanta7768@outlook.com>
2022-09-05 23:36:40 +0530

    Production Ready

--- supabase/functions/collage/index.ts
+++ supabase/functions/collage/index.ts
@@ -2,22 +2,25 @@ import { collageHandler } from "../_router/imgRouter.ts";
 import { Handler, Server } from "../_shared/deps.ts";
 
 const PORT = Deno.env.get("PORT") || 8343;
-const handler: Handler = (req) => {
+const handler: Handler = async (req) => {
   const url = new URL(req.url);
-  if (url.pathname === "/") {
-    collageHandler(req).then((res) => {
-      return res;
+  console.log(url);
+  try {
+    const res = await collageHandler(req);
+    return res;
+  } catch (error) {
+    console.log(error);
+    return new Response("Not found", {
+      status: 404,
+      statusText: "No Route Found",
     });
   }
-  return new Response("Not found", {
-    status: 404,
-    statusText: "No Route Found",
-  });
 };
 
 console.log("Running on a Flash Dinosour </>(-**-)</> ");
 
 console.log(`Listening on: ${PORT}`);
+
 const server = new Server({ port: PORT as number, handler });
 
 await server.listenAndServe();
