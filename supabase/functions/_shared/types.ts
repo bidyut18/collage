a456281930978996c98f2829d9a914d78464ff0b
Bidyut Mahanta <bidyutmahanta7768@outlook.com>
2022-09-07 00:47:53 +0530

    CORS Problem

--- supabase/functions/_shared/types.ts
+++ supabase/functions/_shared/types.ts
@@ -27,7 +27,7 @@ export interface ImageRequest {
 }
 
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://photo-collage.vercel.app/",
+  "Access-Control-Allow-Origin": "https://photo-collage.vercel.app",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, Content-Type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
